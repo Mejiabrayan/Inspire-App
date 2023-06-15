3d7b19558223b57c5b4bf8c1be63652abc51afe7
Brayan Mejia Cuenca <brayanmejiacuenca@Brayans-MacBook-Air.local>
2023-06-15 12:34:05 -0700

    intial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,36 @@
-import '@/styles/globals.css'
+import Navbar from "@/components/Navbar";
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+import { Toaster } from "./Toaster";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+  title: "Breadit",
+  description: "A Reddit clone built with Next.js and TypeScript.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={
+        (cn("bg-white text-slate-900 antialiased light"), inter.className)
+      }
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        {/* @ts-expect-error */}
+        <Navbar />
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
+        <Toaster />
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/components/SignIn.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Icons } from "./Icons";
+import Link from "next/link";
+import UserAuthForm from "./UserAuthForm";
+
+export const SignIn = () => {
+  return (
+    <div className="container mx-auto flex flex-col w-full justify-center space-y-6 sm:w-[400px]">
+      <div className="flex flex-col space-y-2 text-center">
+        <Icons.logo className="mx-auto h-6 w-6" />
+        <h1 className="text-2xl font-semibold tracking-tight">Welcome Back</h1>
+        <p className="text-sm max-w-xs mx-auto">
+          By continuing, you are setting up a breadit account and agreeing to
+          our User Agreement and Privacy Policy.
+        </p>
+        {/* sign in form */}
+        <UserAuthForm  /> 
+        <p className="px-8 text-center text-sm text-zinc-700">
+          New to Breadit?{" "}
+          <Link
+            href="/sign-up"
+            className="hover:text-zinc-800 text-sm underline underline-offset-4"
+          >
+            Sign Up
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
