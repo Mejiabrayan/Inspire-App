fb789fcea7e802ebc66883da25a64958422bcd7c
Your Name <brayanmejiacuenca@Brayans-MacBook-Air.local>
2023-06-30 12:20:33 -0700

    updated favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default async function Home() {
             </p>
           </div>
           <div className='-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6'>
-            <div className='flex justify-between gap-x-4 py-3'>
+            <div className='flex justify-between gap-x-4 py-3 bg-slate-50 '>
               <p className='text-zinc-500'>
                 Your personal Inspire homepage. Come here to check in with your
                 favorite communities.
